ea3db45090896060a3a5e3ae67fe384ee0300c10
Kevin R. Thornton <krthornt@uci.edu>
2023-08-04 10:51:33 -0700

    Fix typo in comment. Fix error in clang-format symlink that only worked by luck.

--- fedora_dev/bootstrap_devel.sh
+++ fedora_dev/bootstrap_devel.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/tskit-dev/tskit
-# 3.11 is the default but is a bit too bleedge edge still
+# 3.11 is the default but is a bit too bleeding edge still
 python3.10 -m venv venv
 . venv/bin/activate
 # no-cache-dir, etc., provide isolation from
@@ -7,6 +7,6 @@ python3.10 -m venv venv
 python -m pip install --no-cache-dir --upgrade pip setuptools clang-format==6.0.1
 python -m pip install --no-cache-dir -r tskit/python/requirements/development.txt
 # Need this for pre-commit to work
-ln -s clang-format $(which clang-format)-6.0
+ln -s clang-format $(which clang-format-6.0)
 cd tskit
 pre-commit install
